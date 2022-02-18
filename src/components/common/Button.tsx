f81af6ccaa6d49adce87c7661c665e958b4400b4
hyunjoebrother <hyunjoe31@gmail.com>
2022-02-18 15:04:46 +0900

    fix: StoryBook Issues & Btn Component

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -3,16 +3,22 @@ import styled from "styled-components";
 
 export interface ButtonProps {
   children: React.ReactNode;
+  onClick?: (e?: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
-function Button({ children }: ButtonProps) {
+function Button({ children, onClick }: ButtonProps) {
   const StyledButton = styled.button`
     height: 2rem;
     width: 5rem;
     background-color: red;
+
+    &:hover {
+      background-color: #000;
+      color: #fff;
+    }
   `;
 
-  return <StyledButton>{children}</StyledButton>;
+  return <StyledButton onClick={onClick}>{children}</StyledButton>;
 }
 
 export default Button;
